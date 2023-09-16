7cefd00fb3a5907c73e8398205613c286125d0ab
Michael Li <michael20051994@gmail.com>
2023-09-16 11:23:44 -0700

    ci/cd scripts

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,16 +1,13 @@
-import { z } from "zod";
+import {z} from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {createTRPCRouter, publicProcedure} from "~/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.example.findMany();
-  }),
+    hello: publicProcedure
+        .input(z.object({text: z.string()}))
+        .query(({input}) => {
+            return {
+                greeting: `Hello ${input.text}`,
+            };
+        })
 });
