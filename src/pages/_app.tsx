8cc0982651872a69ee93817cbf275eadf6ea7866
Michael Li <michael20051994@gmail.com>
2023-09-18 22:38:18 -0700

    setup chakraui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import { type AppType } from "next/app";
+import {type AppType} from "next/app";
 
-import { api } from "~/utils/api";
+import {api} from "~/utils/api";
 
 import "~/styles/globals.css";
+import {ChakraProvider} from "@chakra-ui/react";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({Component, pageProps}) => {
+    return <ChakraProvider>
+        <Component {...pageProps} />
+    </ChakraProvider>;
 };
 
 export default api.withTRPC(MyApp);
