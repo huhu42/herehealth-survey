89c9649eb02e167f01f145e60c8056931f1f7213
Michael Li <michael20051994@gmail.com>
2023-09-18 23:43:51 -0700

    responsive layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import {Center, Heading, Text, VStack} from "@chakra-ui/react";
+import {Center, Flex, Text} from "@chakra-ui/react";
 import Logo from "~/client/components/Logo";
 
 export default function Survey() {
     return (
         <Center w={"100vw"} h={"100vh"} bgGradient={'linear(to-b, purple.900, purple.600)'}>
-            <VStack>
-                <Logo w={"80"}/>
-                <Text fontSize={"lg"} color={"white"} mt={4}>
-                    Find your calling using state-of-the-art psychometric test and AI.
+            <Flex direction={"column"} alignItems={"center"} textAlign="center">
+                <Logo w={{base: "60", md: "80"}}/>
+                <Text w={{base: "80", md: "100"}} fontSize={{base: "md", md: "lg"}} color={"white"} mt={4}>
+                    Find your calling with state-of-the-art psychometrics and AI.
                 </Text>
-            </VStack>
+            </Flex>
         </Center>
     );
 }
