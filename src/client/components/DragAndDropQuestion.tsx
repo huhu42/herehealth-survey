e0d7ce2144dce60ee3587da771af4c6e4375d4a0
Michael Li <michael20051994@gmail.com>
2023-09-22 15:53:41 -0700

    fix drag and drop rendering

--- src/client/components/DragAndDropQuestion.tsx
+++ src/client/components/DragAndDropQuestion.tsx
@@ -1,6 +1,5 @@
-import {Reorder, useMotionValue} from "framer-motion";
+import {Reorder} from "framer-motion";
 import React, {useState} from "react";
-import {useRaisedShadow} from "~/client/useRaisedShadow";
 import {Card, Flex, Text, UnorderedList} from "@chakra-ui/react";
 
 type DragAndDropQuestion = {
@@ -9,26 +8,6 @@ type DragAndDropQuestion = {
 }
 
 export default function DragAndDropQuestion({question, options}: DragAndDropQuestion) {
-    type ReorderItemProps = {
-        text: string;
-    }
-
-    function ReorderItem({text}: ReorderItemProps) {
-        const y = useMotionValue(0);
-        const boxShadow = useRaisedShadow(y);
-
-        return (
-            <Reorder.Item value={text} id={text} style={{boxShadow, y}}>
-                <Card bgGradient='linear(to-tl, purple.400, purple.200)' w={{base: 240, md: 300}} p={4}
-                      mt={{base: 2, md: 4}}>
-                    <Text fontSize="xl" color='black'>
-                        {text}
-                    </Text>
-                </Card>
-            </Reorder.Item>
-        );
-    };
-
     const [items, setItems] = useState(options);
 
     return (
@@ -36,12 +15,23 @@ export default function DragAndDropQuestion({question, options}: DragAndDropQues
             <Text w={{base: "80", md: "100"}}
                   fontSize={{base: "xl", md: "xxl"}}
                   color={"white"}
-                  mb={2}>{question}</Text>
+                  mb={2}>
+                {question}
+            </Text>
             <Reorder.Group axis="y" onReorder={setItems} values={items}>
                 <UnorderedList listStyleType={"none"} pl={0} ml={0}>
                     {items.map((item) => (
-                        <ReorderItem key={item} text={item.toString()}/>
-                    ))}
+                        <Reorder.Item key={item} value={item} id={item}>
+                            <Card bgGradient='linear(to-tl, purple.400, purple.200)'
+                                  w={{base: 240, md: 300}}
+                                  p={4}
+                                  mt={{base: 2, md: 4}}>
+                                <Text fontSize="xl" color='black'>
+                                    {item}
+                                </Text>
+                            </Card>
+                        </Reorder.Item>))
+                    }
                 </UnorderedList>
             </Reorder.Group>
         </Flex>
