2788de385e88db5e69104f5fcdb580ee3a5a7489
Michael Li <michael20051994@gmail.com>
2023-09-29 10:21:25 -0700

    have splash screen logo also direct to the marketing page

--- src/client/components/Splash.tsx
+++ src/client/components/Splash.tsx
@@ -1,34 +1,39 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
+import {Button, Flex, Text} from "@chakra-ui/react";
 import Logo from "~/client/components/Logo";
 import React from "react";
+import {useRouter} from "next/router";
 
 type SplashProps = {
-  onNavigation: () => void;
+    onNavigation: () => void;
 };
 
-export default function Splash({ onNavigation }: SplashProps) {
-  return (
-    <Flex direction={"column"} alignItems={"center"} textAlign="center">
-      <Logo w={{ base: "60", md: "80" }} />
-      <Text
-        w={{ base: "80", md: "100" }}
-        fontSize={{ base: "md", md: "lg" }}
-        color={"white"}
-        mt={4}
-      >
-        Find your calling with state-of-the-art psychometrics and AI.
-      </Text>
-      <Button
-        aria-label={"start-button"}
-        bg="white"
-        mt={8}
-        variant={"solid"}
-        onClick={() => {
-          onNavigation();
-        }}
-      >
-        Let's go!
-      </Button>
-    </Flex>
-  );
+export default function Splash({onNavigation}: SplashProps) {
+    const router = useRouter();
+    return (
+        <Flex direction={"column"} alignItems={"center"} textAlign="center">
+            <Logo w={{base: "60", md: "80"}}
+                  onClick={async () => {
+                      await router.push("https://www.uniphye.com/")
+                  }}/>
+            <Text
+                w={{base: "80", md: "100"}}
+                fontSize={{base: "md", md: "lg"}}
+                color={"white"}
+                mt={4}
+            >
+                Find your calling with state-of-the-art psychometrics and AI.
+            </Text>
+            <Button
+                aria-label={"start-button"}
+                bg="white"
+                mt={8}
+                variant={"solid"}
+                onClick={() => {
+                    onNavigation();
+                }}
+            >
+                Let's go!
+            </Button>
+        </Flex>
+    );
 }
