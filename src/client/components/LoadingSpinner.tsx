4598d249b9d91cd6cb161d13ad085134e129cd13
Michael Li <michael20051994@gmail.com>
2023-10-02 18:33:00 -0700

    add subtext to spinner

--- src/client/components/LoadingSpinner.tsx
+++ src/client/components/LoadingSpinner.tsx
@@ -1,13 +1,21 @@
-import { Spinner } from "@chakra-ui/react";
+import {Text, HStack, Spinner, VStack} from "@chakra-ui/react";
 
 export default function LoadingSpinner() {
-  return (
-    <Spinner
-      thickness="4px"
-      speed="1s"
-      emptyColor="gray.200"
-      color="purple.500"
-      size="xl"
-    />
-  );
+    return (
+        <VStack>
+            <Spinner
+                thickness="4px"
+                speed="1s"
+                emptyColor="gray.200"
+                color="purple.500"
+                mt={8}
+                size="xl"
+            />
+            <Text
+                fontSize={"sm"}
+                color={"gray.200"}
+                mt={8}
+            >This may take up to 20 seconds.</Text>
+        </VStack>
+    );
 }
