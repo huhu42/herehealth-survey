80ba93e5535d3740f0d035f168a3ee61110f56cd
Michael Li <michael20051994@gmail.com>
2023-10-04 08:40:03 -0700

    simplify again

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -3,30 +3,20 @@ import {Card, Center, Flex, Text, UnorderedList} from "@chakra-ui/react";
 import {Reorder} from "framer-motion";
 
 export default function Test() {
-    type ItemKey = 0 | 1 | 2 | 3 | 4 | 5;
-    type DragAndDropItem = {
-        key: ItemKey
-        description: string
-    }
-
-    type DragAndDropQuestionProp = {
-        items: Array<DragAndDropItem>;
-    };
-
     function DragAndDropQuestion() {
-        const [itemsInput, setItemsInput] = useState([{key: 0, description: "Assessing ideas and situations"},
-            {key: 1, description: "Supporting those in need with an idea or project"},
-            {key: 2, description: "Challenging norms and pondering possibilities for potential and opportunity"},
-            {key: 3, description: "Encouraging and inspiring others to take action"},
-            {key: 4, description: "Novelizing new ideas and solutions in response to problems"},
-            {key: 5, description: "Delivering projects and pushing tasks to completion"}]
+        const [itemsInput, setItemsInput] = useState(["Assessing ideas and situations",
+            "Supporting those in need with an idea or project",
+            "Challenging norms and pondering possibilities for potential and opportunity",
+            "Encouraging and inspiring others to take action",
+            "Novelizing new ideas and solutions in response to problems",
+            "Delivering projects and pushing tasks to completion"]
         );
         return (
             <Flex direction={"column"} textAlign={"center"} alignItems={"center"}>
                 <Reorder.Group axis="y" onReorder={setItemsInput} values={itemsInput}>
                     <UnorderedList listStyleType={"none"} pl={0} ml={0}>
                         {itemsInput.map((item) => (
-                            <Reorder.Item key={item.key} value={item} id={item.key.toString()}>
+                            <Reorder.Item key={item} value={item} id={item}>
                                 <Card
                                     bgGradient="linear(to-tl, purple.400, purple.200)"
                                     w={{base: 300, md: 400}}
@@ -36,7 +26,7 @@ export default function Test() {
                                 >
                                     <Center w={"100%"} h={"100%"}>
                                         <Text fontSize={{base: "sm", md: "lg"}} color="white">
-                                            {item.description}
+                                            {item}
                                         </Text>
                                     </Center>
                                 </Card>
