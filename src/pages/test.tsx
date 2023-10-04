c405dd8145b647e697c4540e4d735a6e320d8897
Michael Li <michael20051994@gmail.com>
2023-10-04 08:49:08 -0700

    remove cards, just bullets

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -16,19 +16,7 @@ export default function Test() {
                 <Reorder.Group axis="y" onReorder={setItemsInput} values={itemsInput}>
                     {itemsInput.map((item) => (
                         <Reorder.Item key={item} value={item} id={item}>
-                            <Card
-                                bgGradient="linear(to-tl, purple.400, purple.200)"
-                                w={{base: 300, md: 300}}
-                                h={{base: 12, md: 12}}
-                                px={2}
-                                mt={{base: 2, md: 2}}
-                            >
-                                <Center w={"100%"} h={"100%"}>
-                                    <Text fontSize={{base: "sm", md: "lg"}} color="white">
-                                        {item}
-                                    </Text>
-                                </Center>
-                            </Card>
+                            {item}
                         </Reorder.Item>
                     ))}
                 </Reorder.Group>
