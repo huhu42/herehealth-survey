9ce9caed434f667844ac9130c21f33b2d5b27208
Michael Li <michael20051994@gmail.com>
2023-10-04 08:56:09 -0700

    numeric values, this is the toy example on Framer docs

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,24 +1,24 @@
 import React, {useState} from "react";
-import {Card, Center, Flex, Text, UnorderedList} from "@chakra-ui/react";
+import {Center, Flex} from "@chakra-ui/react";
 import {Reorder} from "framer-motion";
 
 export default function Test() {
     function DragAndDropQuestion() {
-        const [itemsInput, setItemsInput] = useState(["Assessing ideas and situations",
-            "Supporting those in need with an idea or project",
-            "Challenging norms and pondering possibilities for potential and opportunity",
-            "Encouraging and inspiring others to take action",
-            "Novelizing new ideas and solutions in response to problems",
-            "Delivering projects and pushing tasks to completion"]
+        const [itemsInput, setItemsInput] = useState([0,
+            1,
+            2,
+            3,
+            4,
+            5]
         );
         return (
-                <Reorder.Group axis="y" onReorder={setItemsInput} values={itemsInput}>
-                    {itemsInput.map((item) => (
-                        <Reorder.Item key={item} value={item}>
-                            {item}
-                        </Reorder.Item>
-                    ))}
-                </Reorder.Group>
+            <Reorder.Group axis="y" onReorder={setItemsInput} values={itemsInput}>
+                {itemsInput.map((item) => (
+                    <Reorder.Item key={item} value={item}>
+                        {item}
+                    </Reorder.Item>
+                ))}
+            </Reorder.Group>
         );
     }
 
