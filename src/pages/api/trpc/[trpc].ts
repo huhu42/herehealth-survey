c861d6d2d8433b6ab4823d1023b94c7a3f571cc2
Michael Li <michael20051994@gmail.com>
2023-10-12 20:02:42 -0700

    delete the vercel.json since it is not used

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,7 +4,7 @@ import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
-export const maxDuration = 40;
+export const maxDuration = 60;
 
 // export API handler
 export default createNextApiHandler({
